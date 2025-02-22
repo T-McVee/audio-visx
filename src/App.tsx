076d96ca631139d430b2fbed25dbb2e472911b13
Tim McVinish <iam@tmcvee.com>
2025-02-22 11:34:34 +1000

    Installs prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react"
-import useAudio from "./hooks/useAudio";
+import { useState } from 'react';
+import useAudio from './hooks/useAudio';
 import { Bar } from '@visx/shape';
 import { Group } from '@visx/group';
 import { GradientPurpleRed, LinearGradient } from '@visx/gradient';
 import { scaleBand, scaleLinear } from '@visx/scale';
 import { GridRows, GridColumns } from '@visx/grid';
-
 
 const margin = { top: 40, right: 10, bottom: 40, left: 10 };
 const width = typeof window !== 'undefined' ? window.innerWidth : 1600;
@@ -17,7 +16,8 @@ export const accentColor = '#edffea';
 export const accentColorDark = '#75daad';
 
 function App() {
-  const { startFrequencyData, stopFrequencyData, analyzer, frequencyData } = useAudio(); 
+  const { startFrequencyData, stopFrequencyData, analyzer, frequencyData } =
+    useAudio();
 
   const [frequencyBands, setFrequencyBands] = useState(1);
   const [barPadding, setBarPadding] = useState(0.4);
@@ -36,25 +36,68 @@ function App() {
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw', padding: '0 1rem', boxSizing: 'border-box' }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          width: '100vw',
+          padding: '0 1rem',
+          boxSizing: 'border-box',
+        }}
+      >
         <h1>VisX Audio</h1>
-        
+
         <p>{analyzer ? 'Connected' : 'Disconnected'}</p>
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'center', justifyContent: 'center', maxWidth: '100%',  }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            maxWidth: '100%',
+          }}
+        >
           <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
-          <label htmlFor="frequencyBands">Frequency Bands</label>
-          <input type="number" id="frequencyBands" value={frequencyBands} onChange={(e) => setFrequencyBands(Number(e.target.value))} min={1} max={127} />
-          <label htmlFor="barPadding">Bar Padding</label>
-            <input type="range" id="barPadding" value={barPadding} onChange={(e) => setBarPadding(Number(e.target.value))} step={0.01} min={-4} max={1} />
+            <label htmlFor="frequencyBands">Frequency Bands</label>
+            <input
+              type="number"
+              id="frequencyBands"
+              value={frequencyBands}
+              onChange={(e) => setFrequencyBands(Number(e.target.value))}
+              min={1}
+              max={127}
+            />
+            <label htmlFor="barPadding">Bar Padding</label>
+            <input
+              type="range"
+              id="barPadding"
+              value={barPadding}
+              onChange={(e) => setBarPadding(Number(e.target.value))}
+              step={0.01}
+              min={-4}
+              max={1}
+            />
           </div>
           <div style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
-            
             <button onClick={startFrequencyData}>Start Frequency Data</button>
             <button onClick={stopFrequencyData}>Stop Frequency Data</button>
           </div>
-          <div style={{ display: 'flex', width: width, maxWidth: '100%', flexDirection: 'row', gap: '0px', overflow: 'hidden'}}>
+          <div
+            style={{
+              display: 'flex',
+              width: width,
+              maxWidth: '100%',
+              flexDirection: 'row',
+              gap: '0px',
+              overflow: 'hidden',
+            }}
+          >
             <svg width={0} height={0}>
-               <defs>
+              <defs>
                 <linearGradient id="bar-gradient" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="0%" stopColor="#c4b5fd" />
                   <stop offset="50%" stopColor="#8b5cf6" />
@@ -62,11 +105,30 @@ function App() {
                 </linearGradient>
               </defs>
             </svg>
-            <svg width={'100%'} height={height} style={{ borderRadius: '2rem' }}>
+            <svg
+              width={'100%'}
+              height={height}
+              style={{ borderRadius: '2rem' }}
+            >
               <GradientPurpleRed id="teal" />
-              <LinearGradient id="background-gradient" from={background} to={background2} />
-              <LinearGradient id="area-gradient" from={accentColorDark} to={accentColor} toOpacity={0.6} fromOpacity={0.1} />
-              <rect width={'100%'} height={height} fill="url(#background-gradient)"  opacity={1} />
+              <LinearGradient
+                id="background-gradient"
+                from={background}
+                to={background2}
+              />
+              <LinearGradient
+                id="area-gradient"
+                from={accentColorDark}
+                to={accentColor}
+                toOpacity={0.6}
+                fromOpacity={0.1}
+              />
+              <rect
+                width={'100%'}
+                height={height}
+                fill="url(#background-gradient)"
+                opacity={1}
+              />
               <Group top={0}>
                 <GridRows
                   scale={yScale}
@@ -84,15 +146,15 @@ function App() {
                   strokeDasharray="2,2"
                   strokeOpacity={0.5}
                 />
-                {frequencyData && [...frequencyData].map((data, index) => {
-                  const barWidth = xScale.bandwidth();
-                  const barHeight = height - yScale(data) - margin.top;
-                  const barX = xScale(index);
-                  const barY = yScale(data) + margin.top;
-                  
-                  return (
-                  index % frequencyBands === 0
-                    ? <Bar
+                {frequencyData &&
+                  [...frequencyData].map((data, index) => {
+                    const barWidth = xScale.bandwidth();
+                    const barHeight = height - yScale(data) - margin.top;
+                    const barX = xScale(index);
+                    const barY = yScale(data) + margin.top;
+
+                    return index % frequencyBands === 0 ? (
+                      <Bar
                         x={barX}
                         y={barY}
                         height={barHeight}
@@ -100,15 +162,15 @@ function App() {
                         fill="url(#area-gradient)"
                         key={index}
                       />
-                    : null
-                  )})}
+                    ) : null;
+                  })}
               </Group>
             </svg>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hooks/useAudio.tsx
+++ src/hooks/useAudio.tsx
@@ -1,12 +1,11 @@
-import { useRef, useState } from "react";
-import { FrequencyBands } from "../App";
-
-
+import { useRef, useState } from 'react';
+import { FrequencyBands } from '../App';
 
 export default function useAudio() {
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
   const [audioAnalyzer, setAudioAnalyzer] = useState<AnalyserNode | null>(null);
-  const [audioSource, setAudioSource] = useState<MediaStreamAudioSourceNode | null>(null);
+  const [audioSource, setAudioSource] =
+    useState<MediaStreamAudioSourceNode | null>(null);
   const [frequencyData, setFrequencyData] = useState<Uint8Array | null>(null);
   const animationFrameRef = useRef<number | null>(null);
 
@@ -32,7 +31,10 @@ export default function useAudio() {
     return analyzerNodePromise;
   }
 
-  async function connectUsersMic(context: AudioContext, analyzer?: AnalyserNode) {
+  async function connectUsersMic(
+    context: AudioContext,
+    analyzer?: AnalyserNode
+  ) {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       const source = context.createMediaStreamSource(stream);
@@ -44,25 +46,24 @@ export default function useAudio() {
     }
   }
 
-  async function audioContextSetup() { 
+  async function audioContextSetup() {
     if (audioContext && audioAnalyzer && audioSource) return;
 
     try {
-      
-        const context = await createNewAudioContext();
-        setAudioContext(context);
-      
-        const analyzer = await createAnalyzer(context);
-        setAudioAnalyzer(analyzer);
-      
-        const source = await connectUsersMic(context, analyzer);
+      const context = await createNewAudioContext();
+      setAudioContext(context);
+
+      const analyzer = await createAnalyzer(context);
+      setAudioAnalyzer(analyzer);
+
+      const source = await connectUsersMic(context, analyzer);
       setAudioSource(source);
-      
+
       return { context, analyzer, source };
-      } catch (err) {
-        console.error('Error setting up audio context:', err);
-        throw err;
-      }
+    } catch (err) {
+      console.error('Error setting up audio context:', err);
+      throw err;
+    }
   }
 
   function getFrequencyData(analyzer: AnalyserNode) {
@@ -72,14 +73,14 @@ export default function useAudio() {
     return frequencyData;
   }
 
-  async function startFrequencyData() {  
+  async function startFrequencyData() {
     if (isRunning) return;
-    
+
     const audioContext = await audioContextSetup();
     const analyzer = audioContext?.analyzer ?? audioAnalyzer;
 
     if (!analyzer) return;
-    
+
     const updateData = () => {
       const data = getFrequencyData(analyzer!);
       setFrequencyData(data);
@@ -93,7 +94,6 @@ export default function useAudio() {
 
     setIsRunning(true);
   }
-
 
   function stopFrequencyData() {
     if (!isRunning) return;
@@ -111,10 +111,10 @@ export default function useAudio() {
     audioContextSetup,
     startFrequencyData,
     stopFrequencyData,
-    
+
     analyzer: audioAnalyzer,
     source: audioSource,
     context: audioContext,
-    frequencyData
-  }
+    frequencyData,
+  };
 }
